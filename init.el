eaddeb6bd2b7e2bb22170a3989737302614ebfd3
cosmomapanje <cosmo.mapanje@gmail.com>
2015-12-28 20:11:54 +0800

    add org-mode into init.el

--- init.el
+++ init.el
@@ -106,3 +106,10 @@
   "Minor mode that calls `erc-nick-notify-cmd' when his nick gets
 mentioned in an erc channel" t)
 (eval-after-load 'erc '(erc-nick-notify-mode t))
+
+;;; org-mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
