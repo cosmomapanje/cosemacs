5dd5c30656d3730eece951f76b7e09a420f4713e
cosmomapanje <cosmo.mapanje@gmail.com>
2016-01-04 19:05:30 +0800

    add font size

--- other.el
+++ other.el
@@ -6,6 +6,9 @@
 (setq default-frame-alist
       '((height . 35)(width . 80)(menubar-linux . 20)(tool-bar-linux . 0)))
 
+;;; font size
+(set-default-font "Monospace 16")
+
 ;;; org-mode
 ; add new line
 (add-hook 'org-mode-hook (lambda () (setq truncate-line nil)))
